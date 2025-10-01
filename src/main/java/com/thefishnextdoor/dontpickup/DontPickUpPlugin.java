23784fd09045128a12c089f13fb7a3ecca0bb8da
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-10-01 14:52:31 -0500

    refactor: streamline command registration and enhance logging methods

--- src/main/java/com/thefishnextdoor/dontpickup/DontPickUpPlugin.java
+++ src/main/java/com/thefishnextdoor/dontpickup/DontPickUpPlugin.java
@@ -1,5 +1,7 @@
 package com.thefishnextdoor.dontpickup;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -21,10 +23,7 @@ public class DontPickUpPlugin extends JavaPlugin {
 
         loadConfigs();
 
-        DPU dpu = new DPU();
-        PluginCommand dpuCommand = getCommand("dontpickup");
-        dpuCommand.setExecutor(dpu);
-        dpuCommand.setTabCompleter(dpu);
+        registerCommand("dontpickup", new DPU());
 
         PluginManager pluginManager = getServer().getPluginManager();
         pluginManager.registerEvents(new Join(), this);
@@ -32,13 +31,13 @@ public class DontPickUpPlugin extends JavaPlugin {
 
         AutoSave.start();
 
-        getLogger().info("Plugin enabled");
+        logInfo("Plugin enabled");
     }
 
     public void onDisable() {
         AutoSave.stop();
         PlayerProfileManager.saveAll();
-        getLogger().info("Plugin disabled");
+        logInfo("Plugin disabled");
     }
 
     public static void loadConfigs() {
@@ -51,5 +50,33 @@ public class DontPickUpPlugin extends JavaPlugin {
 
     public static Language getLanguage() {
         return language;
+    }
+
+    public static void logInfo(String message) {
+        getInstance().getLogger().info(message);
+    }
+
+    public static void logWarning(String message) {
+        getInstance().getLogger().warning(message);
+    }
+
+    public static void logSevere(String message) {
+        getInstance().getLogger().severe(message);
+    }
+
+    private boolean registerCommand(String commandName, CommandExecutor commandExecutor) {
+        PluginCommand command = getCommand(commandName);
+        if (command == null) {
+            DontPickUpPlugin.logSevere("Command '" + commandName + "' not found in plugin.yml.");
+            return false;
+        }
+
+        command.setExecutor(commandExecutor);
+
+        if (commandExecutor instanceof TabCompleter) {
+            command.setTabCompleter((TabCompleter) commandExecutor);
+        }
+
+        return true;
     }
 }
