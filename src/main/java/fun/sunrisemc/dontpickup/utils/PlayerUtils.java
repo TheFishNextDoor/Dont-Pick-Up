de9c65a1e8a8ace889083c9b01dd0c661812885c
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-11-17 16:47:36 -0600

    cleanup

--- src/main/java/fun/sunrisemc/dontpickup/utils/PlayerUtils.java
+++ src/main/java/fun/sunrisemc/dontpickup/utils/PlayerUtils.java
@@ -12,16 +12,19 @@ public class PlayerUtils {
     public static Optional<Material> getMaterialInHand(@NotNull Player player) {
         PlayerInventory inventory = player.getInventory();
 
+        // Check main hand
         Material mainHandMaterial = inventory.getItemInMainHand().getType();
         if (!mainHandMaterial.isAir()) {
             return Optional.of(mainHandMaterial);
         }
 
+        // Check off hand
         Material offHandMaterial = inventory.getItemInOffHand().getType();
         if (!offHandMaterial.isAir()) {
             return Optional.of(offHandMaterial);
         }
 
+        // No material in either hand
         return Optional.empty();
     }
 }
