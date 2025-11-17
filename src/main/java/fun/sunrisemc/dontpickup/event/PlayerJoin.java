d256d4c22bb99a20a9a114133af458ef673c33a2
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-11-16 22:17:05 -0600

    player join null annotations

--- src/main/java/fun/sunrisemc/dontpickup/event/PlayerJoin.java
+++ src/main/java/fun/sunrisemc/dontpickup/event/PlayerJoin.java
@@ -5,12 +5,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import org.jetbrains.annotations.NotNull;
+
 import fun.sunrisemc.dontpickup.player.PlayerProfileManager;
 
 public class PlayerJoin implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
+    public void onJoin(@NotNull PlayerJoinEvent event) {
         Player player = event.getPlayer();
         PlayerProfileManager.preLoad(player);
     }
